80b00139e2fb85dd068f208f704a46ba33c88964
Tamirat Tezera <tamirattezera8@gmail.com>
2025-06-29 18:13:38 +0300

    Create MySQLServer.py to initalize alx_book_store database

---
+++ MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='pythonuser',
+            password='Tamirat963'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print("Error:", e)
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
+create_database()
