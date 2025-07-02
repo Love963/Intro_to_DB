e5687e5121d71b267e9ab3d72c96dc676f5cde42
Tamirat Tezera <tamirattezera8@gmail.com>
2025-07-02 21:54:01 +0300

    Fix: Insert Customer data into Customers table in task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
-DELETE FROM Customers WHERE customer_id = 1;
+USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
